6e598a719b7d15cb4b8cd24190a650b109605bae
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2022-01-07 00:18:22 +0300

    Add files via upload

---
+++ omni_GS150.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := GS150
+PRODUCT_NAME := omni_GS150
+PRODUCT_BRAND := DEXP
+PRODUCT_MODEL := GS150
+PRODUCT_MANUFACTURER := dexp
+
